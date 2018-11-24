8fe74cc2ea9f81c950143bdc6734be24dbd4a48b
andicoder <3944775+andicoder@users.noreply.github.com>
2018-11-24 14:06:55 +0100

    add volkszaehler from official github

---
+++ volkszaehler/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TARGET=/to_host
+
+# builder image
+docker build -t build ./build 
+
+# prepare sources 
+docker run -v $(pwd)/dist:$TARGET -t build git clone https://github.com/volkszaehler/volkszaehler.org $TARGET
+docker run -v $(pwd)/dist:$TARGET -t build php composer.phar install -d $TARGET --optimize-autoloader --no-dev -vvv
+
+# runtime image
+if [ -z $1 ] || [ "$1" != "build" ] ; then
+	docker build -t andig/volkszaehler -f Dockerfile.amd64 .
+#	docker build -t andig/rpi-volkszaehler -f Dockerfile.rpi .
+fi
