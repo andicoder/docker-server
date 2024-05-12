e8bdf007ca3d5c9a2df2b78b6e6a829207823206
andicoder <3944775+andicoder@users.noreply.github.com>
2024-05-12 20:34:24 +0200

    add paperless env

--- mail/backup_mail.sh
+++
@@ -1,5 +0,0 @@
-
-#! /bin/sh
-/usr/bin/docker run --rm --volume dockerserver_maildata:/var/mail -v "/home/andreas/backup":/backups  tvial/docker-mailserver tar cfz /backups/docker-mailserver.tgz /var/mail
-/usr/bin/docker exec mariadb-***REMOVED***sh -c 'exec mysqldump --all-databases -uroot -p3@_9#oX2bY]z,+Q_' > /home/andreas/backup/all-databases.sql
-
